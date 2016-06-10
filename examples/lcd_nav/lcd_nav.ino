c7cda3f332f68015da30d5a2a8949ac83fd42a39
Jon Black <jon_black@mm.st>
2016-06-10 15:57:33 +0200

    Fix issue with lcd_nav example

--- examples/lcd_nav/lcd_nav.ino
+++ examples/lcd_nav/lcd_nav.ino
@@ -13,14 +13,19 @@
 
 // renderer
 
+// The LCD circuit:
+//    * LCD RS pin to digital pin 8
+//    * LCD Enable pin to digital pin 9
+//    * LCD D4 pin to digital pin 4
+//    * LCD D5 pin to digital pin 5
+//    * LCD D6 pin to digital pin 6
+//    * LCD D7 pin to digital pin 7
+//    * LCD R/W pin to ground
+LiquidCrystal lcd = LiquidCrystal(8, 9, 4, 5, 6, 7);
+
 class MyRenderer : public MenuComponentRenderer
 {
 public:
-    MyRenderer()
-    : lcd(LiquidCrystal(8, 9, 4, 5, 6, 7))
-    {
-    }
-
     virtual void render(Menu const& menu) const
     {
         lcd.clear();
@@ -49,9 +54,6 @@ public:
     {
         lcd.print(menu.get_name());
     }
-
-private:
-    mutable LiquidCrystal lcd;
 };
 MyRenderer my_renderer;
 
@@ -69,21 +71,7 @@ MenuItem mm_mi2("Level 1 - Item 2 (Item)", &on_item2_selected);
 Menu mu1("Level 1 - Item 3 (Menu)");
 MenuItem mu1_mi1("Level 2 - Item 1 (Item)", on_item3_selected);
 
-/*
-The LCD circuit:
- * LCD RS pin to digital pin 8
- * LCD Enable pin to digital pin 9
- * LCD D4 pin to digital pin 4
- * LCD D5 pin to digital pin 5
- * LCD D6 pin to digital pin 6
- * LCD D7 pin to digital pin 7
- * LCD R/W pin to ground
- */
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(8     ,     9 ,      4 ,      5 ,      6 ,      7 );
-
 // Menu callback function
-// In this example all menu items use the same callback.
 
 void on_item1_selected(MenuItem* p_menu_item)
 {
@@ -105,21 +93,6 @@ void on_item3_selected(MenuItem* p_menu_item)
   lcd.print("Item3 Selected  ");
   delay(1500); // so we can look the result on the LCD
 }
-
-
-/*void displayMenu() {*/
-  /*lcd.clear();*/
-  /*lcd.setCursor(0,0);*/
-  /*// Display the menu*/
-  /*Menu const* cp_menu = ms.get_current_menu();*/
-
-  /*//lcd.print("Current menu name: ");*/
-  /*lcd.print(cp_menu->get_name());*/
-  
-  /*lcd.setCursor(0,1);*/
-  
-  /*lcd.print(cp_menu->get_current_component()->get_name());*/
-/*}*/
 
 void serialPrintHelp() {
   Serial.println("***************");
@@ -170,30 +143,16 @@ void setup()
   lcd.begin(16, 2);
 
   serialPrintHelp();
-  Serial.println("Setting up the menu.");
-  // Menu setup
-  /*
-  Menu Structure:
-   -Item1
-   -Item2
-   -Item3
-   --Item1
-   */
+
   ms.get_root_menu().add_item(&mm_mi1);
   ms.get_root_menu().add_item(&mm_mi2);
   ms.get_root_menu().add_menu(&mu1);
   mu1.add_item(&mu1_mi1);
-
-  Serial.println("Menu set");
 
   ms.display();
 }
 
 void loop()
 {
-  // Handle serial commands
   serialHandler();
-
-  // Wait for two seconds so the output is viewable
-  //delay(2000);
 }
