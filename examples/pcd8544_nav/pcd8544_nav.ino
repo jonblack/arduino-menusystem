4b04003b3f4d04ed431a8960c7d356c7f4713061
Jon Black <jon_black@mm.st>
2016-06-11 21:49:06 +0200

    Add Makefile for pcd8544_nav example

--- examples/pcd8544_nav/pcd8544_nav.ino
+++ examples/pcd8544_nav/pcd8544_nav.ino
@@ -13,13 +13,9 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
-// Menu variables
-MenuSystem ms;
-Menu mm("ROOT Title");
-MenuItem mm_mi1("Lvl1-Item1(I)");
-MenuItem mm_mi2("Lvl1-Item2(I)");
-Menu mu1("Lvl1-Item3(M)");
-MenuItem mu1_mi1("Lvl2-Item1(I)");
+#define PCD8544_CHAR_HEIGHT 8
+
+// LCD
 
 // Software SPI (slower updates, more flexible pin options):
 // pin 7 - Serial clock out (SCLK)
@@ -39,14 +35,50 @@ Adafruit_PCD8544 lcd = Adafruit_PCD8544(5, 4, 3);
 // Note with hardware SPI MISO and SS pins aren't used but will still be read
 // and written to during SPI transfer.  Be careful sharing these pins!
 
-#define PCD8544_CHAR_HEIGHT 8
+// Renderer
+
+class MyRenderer : public MenuComponentRenderer
+{
+public:
+    virtual void render(Menu const& menu) const
+    {
+        lcd.clearDisplay();
+        menu.render(*this);
+        menu.get_current_component()->render(*this);
+        lcd.display();
+    }
+
+    virtual void render_menu_item(MenuItem const& menu_item) const
+    {
+        lcd.setCursor(0, 1 * PCD8544_CHAR_HEIGHT);
+        lcd.print(menu_item.get_name());
+    }
+
+    virtual void render_back_menu_item(BackMenuItem const& menu_item) const
+    {
+        lcd.setCursor(0, 1 * PCD8544_CHAR_HEIGHT);
+        lcd.print(menu_item.get_name());
+    }
+
+    virtual void render_numeric_menu_item(NumericMenuItem const& menu_item) const
+    {
+        lcd.setCursor(0, 1 * PCD8544_CHAR_HEIGHT);
+        lcd.print(menu_item.get_name());
+    }
+
+    virtual void render_menu(Menu const& menu) const
+    {
+        lcd.setCursor(0, 0 * PCD8544_CHAR_HEIGHT);
+        lcd.print(menu.get_name());
+    }
+};
+MyRenderer my_renderer;
 
 // Menu callback function
-// In this example all menu items use the same callback.
 
 void on_item1_selected(MenuItem* p_menu_item)
 {
-  lcd.setCursor(0,2*PCD8544_CHAR_HEIGHT);
+  lcd.setCursor(0, 2 * PCD8544_CHAR_HEIGHT);
   lcd.print("Item1 Selectd");
   lcd.display();
   delay(1500); // so we can look the result on the LCD
@@ -54,7 +86,7 @@ void on_item1_selected(MenuItem* p_menu_item)
 
 void on_item2_selected(MenuItem* p_menu_item)
 {
-  lcd.setCursor(0,2*PCD8544_CHAR_HEIGHT);
+  lcd.setCursor(0, 2 * PCD8544_CHAR_HEIGHT);
   lcd.print("Item2 Selectd");
   lcd.display();
   delay(1500); // so we can look the result on the LCD
@@ -62,71 +94,30 @@ void on_item2_selected(MenuItem* p_menu_item)
 
 void on_item3_selected(MenuItem* p_menu_item)
 {
-  lcd.setCursor(0,2*PCD8544_CHAR_HEIGHT);
+  lcd.setCursor(0, 2 * PCD8544_CHAR_HEIGHT);
   lcd.print("Item3 Selectd");
   lcd.display();
   delay(1500); // so we can look the result on the LCD
 }
 
-// Standard arduino functions
 
-void setup()
-{
-  Serial.begin(9600);
-  lcd.begin();
-  // you can change the contrast around to adapt the display
-  // for the best viewing!
-  lcd.setContrast(60);
-  
-  #ifdef HIDE_SPLASH
-  lcd.clearDisplay();
-  #else
-  lcd.display(); // show splashscreen
-  delay(2000);
-  #endif // HIDE_SPLASH
+// Menu variables
 
-  lcd.setTextSize(1);
-  
-  serialPrintHelp();
-  Serial.println("Setting up the menu.");
-  // Menu setup
-  /*
-  Menu Structure:
-   -Item1
-   -Item2
-   -Item3
-   --Item1
-   
-   */
-  mm.add_item(&mm_mi1, &on_item1_selected);
-  mm.add_item(&mm_mi2, &on_item2_selected);
-  mm.add_menu(&mu1);
-  mu1.add_item(&mu1_mi1, &on_item3_selected);
-  ms.set_root_menu(&mm);
-  Serial.println("Menu setted.");
-  displayMenu();
-}
+MenuSystem ms(my_renderer);
+MenuItem mm_mi1("Lvl1-Item1(I)", &on_item1_selected);
+MenuItem mm_mi2("Lvl1-Item2(I)", &on_item2_selected);
+Menu mu1("Lvl1-Item3(M)");
+MenuItem mu1_mi1("Lvl2-Item1(I)", &on_item3_selected);
 
-void loop()
-{
-  // Handle serial commands
-  serialHandler();
-
-  // Wait for two seconds so the output is viewable
-  //delay(2000);
-}
-
-void displayMenu() {
-  lcd.clearDisplay();
-  lcd.setCursor(0,0*PCD8544_CHAR_HEIGHT);
-  // Display the menu
-  Menu const* cp_menu = ms.get_current_menu();
-
-  //lcd.print("Current menu name: ");
-  lcd.print(cp_menu->get_name());
-  lcd.setCursor(0,1*PCD8544_CHAR_HEIGHT);
-  lcd.print(cp_menu->get_current_component()->get_name());
-  lcd.display();
+void serialPrintHelp() {
+  Serial.println("***************");
+  Serial.println("w: go to previus item (up)");
+  Serial.println("s: go to next item (down)");
+  Serial.println("a: go back (right)");
+  Serial.println("d: select \"selected\" item");
+  Serial.println("?: print this help");
+  Serial.println("h: print this help");
+  Serial.println("***************");
 }
 
 void serialHandler() {
@@ -135,19 +126,19 @@ void serialHandler() {
     switch (inChar) {
     case 'w': // Previus item
       ms.prev();
-      displayMenu();
+      ms.display();
       break;
     case 's': // Next item
       ms.next();
-      displayMenu();
+      ms.display();
       break;
     case 'a': // Back pressed
       ms.back();
-      displayMenu();
+      ms.display();
       break;
     case 'd': // Select pressed
       ms.select();
-      displayMenu();
+      ms.display();
       break;
     case '?':
     case 'h': // Display help
@@ -159,15 +150,34 @@ void serialHandler() {
   }
 }
 
-void serialPrintHelp() {
-  Serial.println("***************");
-  Serial.println("w: go to previus item (up)");
-  Serial.println("s: go to next item (down)");
-  Serial.println("a: go back (right)");
-  Serial.println("d: select \"selected\" item");
-  Serial.println("?: print this help");
-  Serial.println("h: print this help");
-  Serial.println("***************");
+// Standard arduino functions
 
+void setup()
+{
+  Serial.begin(9600);
+
+  lcd.begin();
+  lcd.setContrast(60);
+
+#ifdef HIDE_SPLASH
+  lcd.clearDisplay();
+#else
+  lcd.display();
+  delay(2000);
+#endif
+
+  lcd.setTextSize(1);
+
+  serialPrintHelp();
+
+  ms.get_root_menu().add_item(&mm_mi1);
+  ms.get_root_menu().add_item(&mm_mi2);
+  ms.get_root_menu().add_menu(&mu1);
+  mu1.add_item(&mu1_mi1);
+  ms.display();
 }
 
+void loop()
+{
+  serialHandler();
+}
